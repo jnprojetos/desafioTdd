dfcbeba99c92a63eba1220c93f0839a1543acf0d
Josimar Jose Nepomuceno <josimar.n2007@gmail.com>
2022-05-02 15:55:02 -0300

    Criação do projeto

---
+++ src/main/java/com/gft/DesafioTddApplication.java
@@ -0,0 +1,13 @@
+package com.gft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTddApplication.class, args);
+	}
+
+}
